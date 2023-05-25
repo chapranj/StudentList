9d708a396f147f0c72aadcb00ff5d4dd7f9f882e
chapranj <chapranj@sheridancollege.ca>
2023-05-25 19:17:13 -0400

    Merge origin/master
    Conflicts:
    	src/firstnetbeans/FirstNetBeans.java

---
+++ src/firstnetbeans/Part_time_student.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package firstnetbeans;
+
+/**
+ *
+ * @author pranj
+ */
+public class Part_time_student extends FirstNetBeans{
+    String name;
+    public void justAMethod(){
+        System.out.println("A new method in different class!");
+    }
+    
+}
